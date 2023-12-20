dcc52201b3d57ef6ddc6891d8767169ea10cf03c
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-20 15:24:33 +0530

    Creating a new table in payroll service

--- employeePayroll.sql
+++ employeePayroll.sql
@@ -3,3 +3,16 @@ CREATE DATABASE payroll_service;
 
 -- Switch to the payroll_service database
 USE payroll_service;
+
+-- Create a table named employee_payroll to store employee information
+CREATE TABLE employee_payroll
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- Employee ID, auto-incremented
+    name VARCHAR(150) NOT NULL,               -- Employee name
+    salary DOUBLE NOT NULL,                   -- Employee salary
+    start DATE NOT NULL,                      -- Start date of employment
+    PRIMARY KEY (id)                          -- Define id as the primary key
+);
+
+-- Display the structure of the employee_payroll table
+DESCRIBE employee_payroll;
